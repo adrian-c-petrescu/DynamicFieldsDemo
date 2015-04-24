b61b67bbdc9aef6fa46ef6d3d4ded6d11fdc04b6
Adrian Petrescu <adrian.petrescu@euromoneyplc.com>
2015-04-24 14:22:11 +0100

    Added new sample json

---
+++ DynamicFieldsDemo/Code/ViewModels/DynamicFormViewModel.cs
@@ -0,0 +1,41 @@
+﻿using DynamicFieldsDemo.Code.DocRepo;
+using DynamicFieldsDemo.Code.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DynamicFieldsDemo.Controllers;
+
+namespace DynamicFieldsDemo.Code.ViewModels
+{
+    //[ModelBinder(typeof(ModelBinder))]
+    public class DynamicFormViewModel
+    {
+        public IEnumerable<AbstractFieldViewModel> FieldViewModels { get; private set; }
+        public Dictionary<string, string> FormValues { get; private set; }
+
+        public DynamicFormViewModel()
+        {   //used on get
+            FormValues = new Dictionary<string, string>();
+            foreach (var vm in FieldViewModels)
+                FormValues[vm.Key] = "";
+
+            //hack to get this thing working
+            FieldViewModels =
+                new RegFormLogic(new FieldContainerRepo(), new FormRepo())
+                    .GetFields("123")
+                    .Select(fieldVm => RegFormController.GetViewModelFromField(fieldVm, FormValues))
+                    .ToArray();
+        }
+    }
+
+    //public class DynFormModelBinder : IModelBinder
+    //{
+
+    //    public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+    //    {
+    //        bindingContext.
+    //    }
+    //}
+}
